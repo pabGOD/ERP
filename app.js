e7ac384a2d720b8e0192423c753c19da48c29b4c
pabGOD <pabllo.freitas@hotmail.com>
2026-02-01 13:56:06 -0300

    ja fiz o front e uma rota cadastrar

--- app.js
+++ app.js
@@ -10,10 +10,22 @@ const mysql = require('mysql2')
 //app
 const app = express();
 
+// Adiconei o bootstrapzada aq ok?
+app.use('bootstrap', express.static('./node_modules/bootstrap/dist'));
+
+// Adiconar o Css ok?
+app.use('/css', express.static('./css'))
+
+
 //Configuração do express-handlebars
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
+
+//manipulação de dados via rotas
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
 
 // config de conexao 
@@ -33,6 +45,13 @@ console.log ('conexao efetuada com tudo tranquilo')
 // Rota principal
 app.get('/', function (req, res){
     res.render('formulario');
+})
+
+//Rota de Cadastro 
+app.post('/cadastrar', (req, res) =>{
+    console.log(req.body);
+    res.end();
+
 })
 
 
